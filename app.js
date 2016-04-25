4a5d9952de8139882d5487653a2640cdef49246b
Jeison <yeison_andres94@hotmail.com>
2016-04-25 15:22:51 -0500

    CONFIGIRATION SERVER OREGON

--- app.js
+++ app.js
@@ -12,7 +12,15 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 //set port
-var port =  8080; 
+var portlisten =  3000; 
+
+//Configure connection
+var username = 'yeison94';
+var passwordconnectingserver = '3137203715yeison94'; 
+var host = 'localhost';
+var port = '4929';
+var databaserver = 'users';
+
 
 
 // ROUTES 
@@ -21,7 +29,7 @@ var routes = require("./routes/users.js")(app);
 
 
 //connect with database users 
-mongoose.connect('mongodb://localhost/users', function(err, res) {
+mongoose.connect('mongodb://'+username+':'+passwordconnectingserver+'@'+host+':'+port+'/'+databaserver, function(err, res) {
 	if(err) {
 		console.log('Error connecting to Database. ' + err);
 	} else {
@@ -34,6 +42,6 @@ mongoose.connect('mongodb://localhost/users', function(err, res) {
 
 
 
-var server = app.listen(port, function () {
+var server = app.listen(portlisten, function () {
     console.log("Listening on port %s...", server.address().port);
 });
